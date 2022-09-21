747e988a61cceff040c00ef960c09909daf06a78
Sam T <97193731+MikeyPancake@users.noreply.github.com>
2022-09-20 22:03:36 -0500

    Assignment8

--- TrevinoAssignment8.java
+++ TrevinoAssignment8.java
@@ -1,3 +1,16 @@
+/*
+Samuel Trevino
+September 20th, 2022
+MOD 8 Assignment
+
+Create a class titled <your first name here> ThreeThreads.
+In this class, you are to use three threads to output three types of characters to a text area for display.
+    -In the first thread, you are to output random letter characters such as a, b, c, d …
+    -In the second thread, you are to output random number digits such as 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    -In the third thread, you are to output random characters such as !, @, #, $, %, &, *
+Display a minimum of 10,000 of each of the three sets.
+ */
+
 import java.lang.Thread;
 import java.util.Random;
 
